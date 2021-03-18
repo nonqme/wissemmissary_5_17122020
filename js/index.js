bb6b3877bea77e26c31f33bff5e7b6778bbb2e69
Wissem Missary <wissem.missary@gmail.com>
2021-03-18 03:22:44 +0100

    apistart

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const teddiesPromise = fetch("http://localhost:3000/api/teddies");
 teddiesPromise
-    .then(data => data.json())
+    .then(response => response.json())
     .then(data => {
-        data.forEach((teddies) => {
-            console.log(teddies);
-        });
+        console.log(data);
+        let affichageTeddies = "<div class=container_grid>";
+        for(let teddies of data){
+            let creationdivTeddies = `<div class=container_grid_bloc><div class=container_grid_bloc_imgdiv><img class=container_grid_bloc_imgdiv_img src=${teddies.imageUrl}></img></div><div class=container_grid_bloc_name><h1 class=container_grid_bloc_name_title>${teddies.name}</h1></div><div class=container_grid_bloc_desc><p class=container_grid_bloc_desc_txt>${teddies.description}</p></div><div class=container_grid_bloc_price><p class=container_grid_bloc_price_txt>${teddies.price}€</p></div></div>`
+            affichageTeddies += creationdivTeddies;
+        }
+        affichageTeddies += "</div>";
+        document.getElementById("container").innerHTML = affichageTeddies;
     })
     .catch((error) => {
         console.log(error);
