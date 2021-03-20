fbf211fbf4c5f47f80fc5fa25605ca6311699686
Wissem Missary <wissem.missary@gmail.com>
2021-03-20 06:44:36 +0100

    effetjs

--- js/index.js
+++ js/index.js
@@ -1,29 +1,66 @@
 const teddiesPromise = fetch("http://localhost:3000/api/teddies");
 teddiesPromise
     .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        let affichageTeddies = "<div class=container_grid>";  
+    .then(response => {
+        let affichageTeddies = "<ul class=container_grid>";  
         let i=0;  
-        for(let teddies of data){
+        for(let teddies of response){
             console.log(teddies)
             i=i+1;
-            let creationdivTeddies = `<div class=container_grid_bloc><div class=container_grid_bloc_imgdiv><img class="container_grid_bloc_imgdiv_img container_grid_bloc_imgdiv_img-style${(i)}" src=${teddies.imageUrl}></img></div><div class=container_grid_bloc_nameprice><div class=container_grid_bloc_nameprice_name><h1 class=container_grid_bloc_nameprice_name_title>${teddies.name}</h1></div><div class=container_grid_bloc_nameprice_desc><p class=container_grid_bloc_nameprice_desc_txt>${teddies.description}</p></div><div class=container_grid_bloc_nameprice_price><p class=container_grid_bloc_nameprice_price_txt>${teddies.price}€</p></div><div class=container_grid_bloc_nameprice_info><p class=container_grid_bloc_nameprice_info_txt>Plus d'info</p></div></div></div>`
+            let creationdivTeddies=`<li class=container_grid_bloc>
+                                        <div class=container_grid_bloc_imgdiv>
+                                            <img class="container_grid_bloc_imgdiv_img container_grid_bloc_imgdiv_img-style${i}" src=${teddies.imageUrl}></img>
+                                        </div>
+                                        <div class=container_grid_bloc_nameprice>
+                                            <div class=container_grid_bloc_nameprice_name>
+                                                <h1 class=container_grid_bloc_nameprice_name_title>${teddies.name}</h1>
+                                            </div>
+                                            <div class=container_grid_bloc_nameprice_desc>
+                                                <p class=container_grid_bloc_nameprice_desc_txt>${teddies.description}</p>
+                                            </div>
+                                            <div class=container_grid_bloc_nameprice_price>
+                                                <p class=container_grid_bloc_nameprice_price_txt>${teddies.price}€</p>
+                                            </div>
+                                            <div class="container_grid_bloc_nameprice_info" >
+                                                <i class="fas fa-chevron-up container_grid_bloc_nameprice_info_iconup"></i>
+                                                <i class="fas fa-chevron-down container_grid_bloc_nameprice_info_icondown"></i>
+                                                <div class="container_grid_bloc_nameprice_info_linkbloc">
+                                                    <a href="" class="container_grid_bloc_nameprice_info_linkbloc_link">Plus d'info</a>
+                                                </div>
+                                            </div>   
+                                    </li>`;
             affichageTeddies += creationdivTeddies;
         }
-        affichageTeddies += "</div>";
+        affichageTeddies += "</ul>";
         document.getElementById("container").innerHTML = affichageTeddies;
-        let test = document.querySelectorAll(".container_grid_bloc");
-        for(let truc of test){
-            console.log(truc);
-            truc.addEventListener('click', event => {
-                truc.style.transform = "scale(1.1,1.1)"
-              })
-        }
+        return response;
+
+    })
+    .then(response => {
+        let iconList = document.querySelectorAll(".container_grid_bloc_nameprice_info_iconup");
+        for (var i = 0; i < iconList.length; i++) {
+            iconList[i].addEventListener("click", function() {
+                let test = this.parentNode;
+                test.classList.add("container_grid_bloc_nameprice_info-openstyle");
+            });
+          }
+        let iconLists = document.querySelectorAll(".container_grid_bloc_nameprice_info_icondown");
+        for (var i = 0; i < iconLists.length; i++) {
+              iconLists[i].addEventListener("click", function() {
+                  let test = this.parentNode;
+                  test.classList.remove("container_grid_bloc_nameprice_info-openstyle");
+              });
+            }  
     })
     .catch((error) => {
-        console.log(error);
+        console.log(error);        
     });
+
+
+
+
+
+
 
 
 
