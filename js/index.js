dd47558d7724231034e3e923c2e39bbbdfe60e24
Wissem Missary <wissem.missary@gmail.com>
2021-03-24 00:21:10 +0100

    ctadone

--- js/index.js
+++ js/index.js
@@ -1,47 +1,48 @@
-const teddiesPromise = fetch("http://localhost:3000/api/teddies");
-teddiesPromise
-    .then(response => response.json())
-    .then(response => {
-        let affichageTeddies = "<ul class=maincontainer_grid>";  
-        let i=0;  
-        for(let teddies of response){
-            console.log(teddies)
-            i=i+1;
-            let creationdivTeddies=`<li class=maincontainer_grid_bloc>
-                                        <a href="" class=maincontainer_grid_bloc_link>
-                                            <article class=maincontainer_grid_bloc_link_article>
-                                                <div class=maincontainer_grid_bloc_link_article_imgdiv>
-                                                    <img class="maincontainer_grid_bloc_link_article_imgdiv_img maincontainer_grid_bloc_link_article_imgdiv_img-style${i}" src=${teddies.imageUrl} alt="Photo d'un ours en peluche prénommé ${teddies.name} en vente sur Orinoco pour ${teddies.price}€."></img>
-                                                </div>
-                                                <div class=maincontainer_grid_bloc_link_article_nameprice>
-                                                    <div class=maincontainer_grid_bloc_link_article_nameprice_name>
-                                                        <h1 class=maincontainer_grid_bloc_link_article_nameprice_name_title>${teddies.name}</h1>
-                                                    </div>
-                                                    <div class=maincontainer_grid_bloc_link_article_nameprice_desc>
-                                                        <p class=maincontainer_grid_bloc_link_article_nameprice_desc_txt>${teddies.description}</p>
-                                                    </div>
-                                                    <div class=maincontainer_grid_bloc_link_article_nameprice_price>
-                                                        <p class=maincontainer_grid_bloc_link_article_nameprice_price_txt>${teddies.price}€</p>
-                                                    </div>
-                                                </div>
-                                            </article>
-                                        </a>   
-                                    </li>`;
-            affichageTeddies += creationdivTeddies;
-        }
-        affichageTeddies += "</ul>";
-        document.getElementById("maincontainer").innerHTML = affichageTeddies;
-    })
-    .catch((error) => {
-        console.log(error);        
-    });
+const teddiesPromise = fetch("http://localhost:3000/api/teddies"); // Appel de l'API
+teddiesPromise  .then(response => response.json())
+                .then(response => {
+                    let affichageTeddies = "<ul class=main_sectionarticle_container_grid>";  
+                    let i=0;  
+                    for(let teddies of response){
+                        console.log(teddies)
+                        i=i+1;
+                        let creationdivTeddies=`<li class=main_sectionarticle_container_grid_bloc>
+                                                    <a href="" class="main_sectionarticle_container_grid_bloc_link">
+                                                        <article class=main_sectionarticle_container_grid_bloc_link_article>
+                                                            <div class=main_sectionarticle_container_grid_bloc_link_article_imgdiv>
+                                                                <img class="main_sectionarticle_container_grid_bloc_link_article_imgdiv_img main_sectionarticle_container_grid_bloc_link_article_imgdiv_img-style${i}" src=${teddies.imageUrl} alt="Photo d'un ours en peluche prénommé ${teddies.name} en vente sur Orinoco pour ${teddies.price}€."></img>
+                                                            </div>
+                                                            <div class=main_sectionarticle_container_grid_bloc_link_article_nameprice>
+                                                                <div class=main_sectionarticle_container_grid_bloc_link_article_nameprice_name>
+                                                                    <h3 class=main_sectionarticle_container_grid_bloc_link_article_nameprice_name_title>${teddies.name}</h3>
+                                                                </div>
+                                                                <div class=main_sectionarticle_container_grid_bloc_link_article_nameprice_desc>
+                                                                    <p class=main_sectionarticle_container_grid_bloc_link_article_nameprice_desc_txt>${teddies.description}</p>
+                                                                </div>
+                                                                <div class=main_sectionarticle_container_grid_bloc_link_article_nameprice_price>
+                                                                    <p class=main_sectionarticle_container_grid_bloc_link_article_nameprice_price_txt>${teddies.price}€</p>
+                                                                </div>
+                                                            </div>
+                                                        </article>
+                                                    </a>   
+                                                </li>`;
+                        affichageTeddies += creationdivTeddies;
+                    }
+                    affichageTeddies += "</ul>";
+                    document.querySelector(".main_sectionarticle_container").innerHTML = affichageTeddies;
+                })
+                .catch((error) => {
+                    console.log(error);        
+                });
 
 const navSlide = () => {
-    const burger = document.querySelector(".headercontainer_mobilemenu_burger");
-    const nav = document.querySelector(".headercontainer_ul");
-    const navLinks = document.querySelectorAll(".headercontainer_ul_list");
-burger.addEventListener("click",() => {
-        nav.classList.toggle("headercontainer_ul-active");
+    const burger = document.querySelector(".header_container_mobilemenu_burger");
+    const nav = document.querySelector(".header_container_ul");
+    const navLinks = document.querySelectorAll(".header_container_ul_list");
+    const blurEffect = document.querySelector(".header_container_mobileeffect")
+    burger.addEventListener("click",() => {
+        nav.classList.toggle("header_container_ul-active");
+        blurEffect.classList.toggle("header_container_mobileeffect-effect")
         navLinks.forEach((link, index) => {
             if(link.style.animation){
                 link.style.animation ="";
@@ -49,9 +50,8 @@ burger.addEventListener("click",() => {
                 link.style.animation = `navFade 0.5s ease forwards ${index / 4 + 0.2}s`;
             }
         });
-        burger.classList.toggle("headercontainer_mobilemenu_burger-toggle");
+        burger.classList.toggle("header_container_mobilemenu_burger-toggle");
     });
-
 }
 navSlide();
 
